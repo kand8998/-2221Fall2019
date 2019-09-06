6cf45877c35d2b620571f2908ba4db1654c63b4b
Kaitlyn <kand8998@gmail.com>
2019-09-06 15:56:20 -0600

    White boxing Solar Panels

---
+++ Snapshot/Assets/Scripts/MonoEvents.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEvents : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent;
+
+    private void Start()
+    {
+    }
+    
+   private void Update()
+    {
+    }
+
+   private void OnTriggerEnter(Collider other)
+   {
+       triggerEnterEvent.Invoke();
+   }
+}
