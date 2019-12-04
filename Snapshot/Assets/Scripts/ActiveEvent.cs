6c2c822067b7e97f85ba383310cc0cc836a80d84
Kaitlyn <kand8998@gmail.com>
2019-12-04 16:03:46 -0700

    Water, Adjustments, Event Explosion

---
+++ Snapshot/Assets/Scripts/ActiveEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ActiveEvent : MonoBehaviour
+{
+    public UnityEvent activeEvent;
+    public bool active;
+    
+    private void Update()
+    {
+        if (active == true)
+        {
+            activeEvent.Invoke();
+        }
+    }
+    
+}
